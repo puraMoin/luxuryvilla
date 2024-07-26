54803954313838d886a700faa2a1f60e2a49a6f1
puraVrushab <vrushab@puratech.in>
2024-07-26 16:34:55 +0530

    made agent and comployee controller and model

---
+++ app/Models/AgentList.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AgentList extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name', 'username', 'email','contact_no','active','created_at','updated_at'
+    ];
+}
