29bf58878695c9a8f7e71e79873c1b15d95125d7
puraVrushab <vrushab@puratech.in>
2024-08-01 10:26:09 +0530

    added model configs

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name', 'description', 'full_view', 'full_add', 'full_edit', 'full_delete', 'super_config',
+        'config', 'created_by', 'modified_by', 'created_at', 'updated_at'
+    ];
 }
