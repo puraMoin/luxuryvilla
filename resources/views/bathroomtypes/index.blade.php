9826cafaf0090575f1985cd256087d193bf34d37
puraVrushab <vrushab@puratech.in>
2024-08-07 11:06:08 +0530

    added masters of bedtypes and bathroomtypes

---
+++ resources/views/bathroomtypes/index.blade.php
@@ -0,0 +1,87 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="section">
+        <div class="container-fluid">
+            <!-- BreadCrumb -->
+            @include('partials.breadcrumb')
+            <!-- Add New Button -->
+            <div class="right-mob-left">
+                <a href="{{ route('bathroomtypes.create') }}">
+                    <button type="button" class="main-btn primary-btn btn-hover btn-xs">Create</button>
+                </a>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="card-style mt-20">
+                <div class="row">
+                    <!-- Add any additional rows or content here -->
+                </div>
+                <div class="table-wrapper table-responsive mt-10">
+                    <table class="table striped-table">
+                        <thead>
+                            <tr>
+                                <th>
+                                    <h6>Name</h6>
+                                </th>
+
+                                {{-- <th class="text-center">
+                                    <h6>Alias</h6>
+                                </th> --}}
+
+                                <th class="text-center">
+                                    <h6>Status</h6>
+                                </th>
+
+                                <th class="text-center">
+                                    <h6>Actions</h6>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($bathroomtype as $bathroomtype)
+                                @php
+                                    $class = '';
+                                    $data = '';
+                                @endphp
+
+                                <tr>
+                                    <td>
+                                        <p>{{ $bathroomtype->name }}</p>
+                                    </td>
+
+                                    {{-- <td class="text-center">
+                                        <p>{{ $bathroomtype->alias }}</p>
+                                    </td> --}}
+
+                                    <td class="text-center">
+                                        @php
+                                          if($bathroomtype->active == '1'){
+                                            $class = 'activelabel';
+                                            $data = 'Active';
+                                          } else {
+                                            $class = 'inactivelabel';
+                                            $data = 'Inactive';
+                                          }
+                                        @endphp
+                                        <div class="{{ $class }}">{{ $data }}</div>
+                                      </td>
+
+
+                                    <td class="text-center">
+                                        <a href="{{ route('bathroomtypes.edit', $bathroomtype->id) }}">
+                                            <i class="lni lni-pencil-alt"></i>
+                                        </a>
+                                        <a href="{{ route('bathroomtypes.show', $bathroomtype->id) }}">
+                                            <i class="lni lni-list"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
