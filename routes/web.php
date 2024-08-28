2dc977518ba22dd88b29d7754186ab1d15309443
puraVrushab <vrushab@puratech.in>
2024-08-28 15:16:56 +0530

    .

--- routes/web.php
+++ routes/web.php
@@ -68,6 +68,7 @@ use App\Http\Controllers\SupplierAccessToCompaniesController;
 use App\Http\Controllers\SupplierBanksController;
 use App\Http\Controllers\SupplierCancellationPoliciesController;
 use App\Http\Controllers\SupplierContactsController;
+use App\Http\Controllers\SupplierExtranetsController;
 use App\Http\Controllers\SupplierPaymentPoliciesController;
 use App\Http\Controllers\SuppliersController;
 use App\Http\Controllers\TaxAccessToCompaniesController;
@@ -205,6 +206,7 @@ Route::get('/dashboard', [DashboardsController::class, 'index'])->name('dashboar
   Route::resource('suppliercancellationpolicies', SupplierCancellationPoliciesController::class);
   Route::resource('suppliercontacts', SupplierContactsController::class);
   Route::resource('supplierbanks', SupplierBanksController::class);
+  Route::resource('supplierextranets', SupplierExtranetsController::class);
 
   Route::get('/get-states/{countryId}', [StatesController::class, 'getStates']);
   Route::get('/get-cities/{stateId}', [CitiesController::class, 'getCities']);
